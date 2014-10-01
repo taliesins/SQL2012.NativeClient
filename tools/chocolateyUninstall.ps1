78306702029c5a1dd24b73bd390e93afb0714ef8
Rory Plaire <codekaizen@gmail.com>
2014-09-30 17:35:01 -0700

    Updating to current sqlncli
    Also fixing package name and uninstall script

--- tools/chocolateyUninstall.ps1
+++ tools/chocolateyUninstall.ps1
@@ -1,12 +1,12 @@
 # HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\
-$msiId = ''
-$msiId64 = ''
+$msiId = '{83C7F964-AC58-4104-B613-B4D0F61DA8CD}'
+$msiId64 = '{D411E9C9-CE62-4DBF-9D92-4CB22B750ED5}'
 
 $package = 'SQL2012.NativeClient'
 
 try {
   
-  if ([IntPtr]::Size -eq 8) { Uninstall-ChocolateyPackage $package 'MSI' -SilentArgs "$msiId64 /qb" -validExitCodes @(0) }
+  if ([Environment]::IsIs64BitOperatingSystem) { Uninstall-ChocolateyPackage $package 'MSI' -SilentArgs "$msiId64 /qb" -validExitCodes @(0) }
   else { Uninstall-ChocolateyPackage $package 'MSI' -SilentArgs "$msiId /qb" -validExitCodes @(0) }
   # the following is all part of error handling
   Write-ChocolateySuccess $package
